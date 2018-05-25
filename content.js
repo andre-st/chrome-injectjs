2571d78ebce048cd260549750b2ead5d470f9059
André <datakadabra@gmail.com>
2018-05-25 16:31:17 +0200

    Update content.js

--- content.js
+++ content.js
@@ -1,3 +1,7 @@
+// Content scripts and the website share the DOM but no javascript functions
+// https://developer.chrome.com/extensions/content_scripts#isolated_world
+// Nevertheless, beware of XSS.
+
 chrome.storage.sync.get( ["mixinsScript", "mixinsState"], stored =>
 {
 	if( stored.mixinsState == "mixinsDisabledState" ) return;
