53e9b5c4bd112db7607fc421214a27fc61f6bd38
andre-st <datakadabra@gmail.com>
2018-05-27 22:01:49 +0200

    Allow breaking JS isolation (calling JS functions of the target Website)

--- content.js
+++ content.js
@@ -6,12 +6,29 @@ chrome.storage.sync.get( ["mixinsScript", "mixinsState"], stored =>
 {
 	if( stored.mixinsState == "mixinsDisabledState" ) return;
 	
-	function mixin( theUrl, theCallback )
+	function mixin( theUrl, theCallback, isIsolated )
 	{
-		if( location.href.startsWith( theUrl ) )
+		if( !location.href.startsWith( theUrl ) ) return;
+		if( isIsolated || true )
+		{
 			theCallback();
+		}
+		else
+		{
+			// This breaks the Javascript isolation but allows to
+			// call Javascript functions of the target Website
+			const s = document.createElement( "script" );
+			s.textContent = "(" + theCallback + ")();";
+			( document.head || document.documentElement ).appendChild( s );
+			s.remove();
+		}
+	}
+	
+	function mixinUnsafe( theUrl, theCallback )
+	{
+		mixin( theUrl, theCallback, false );
 	};
 	
-	eval( stored.mixinsScript );  // Script calls mixin() multiple times
+	eval( stored.mixinsScript );  // Script calls mixinXXX() multiple times
 });
 
