9328041ca9590cf65d56dd810b7637c303682631
andre-st <datakadabra@gmail.com>
2018-08-17 23:17:25 +0200

    Optionally run mixin() with higher privs (CORS)

--- content.js
+++ content.js
@@ -2,19 +2,24 @@ chrome.storage.sync.get( ["mixinsScript", "mixinsState"], stored =>
 {
 	if( stored.mixinsState == "mixinsDisabledState" ) return;
 	
-	
 	function redir( theRegex, theReplace ) { /* implemented in background.js */ };
 	
-	
-	function mixin( theUrl, theCallback )
+	function mixin( theUrl, theCallback, theOpts )
 	{
 		if( !location.href.startsWith( theUrl ) ) return;
+		
+		if( theOpts.runAsContentScript || false )  // More privileges, e.g., CORS doesn't matter
+		{
+			theCallback();
+			return;
+		}
 		
 		// Content scripts and the website share the DOM but no JS functions.
 		// https://developer.chrome.com/extensions/content_scripts#isolated_world
 		// We run user scripts in the world of the target website.
-		// This allows us to call the website's Javascript functions, and
-		// there's no good reason to have it access our extension API.
+		// This allows us to call the website's Javascript functions
+		// and to strip off some privileges: the remote page cannot access
+		// our extension.
 		
 		const s = document.createElement( "script" );
 		s.textContent = "(" + theCallback + ")();";
